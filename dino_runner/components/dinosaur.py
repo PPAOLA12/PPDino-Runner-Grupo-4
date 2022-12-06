65dec6f3d4eae57aaca580662ce9fd2dd996c8e0
PPAOLA12 <Paola.Paredes@fundacion-jala.org>
2022-12-06 09:49:06 -0400

    Class 1: Dinosaur duck (tarea)

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -7,6 +7,7 @@ class Dinosaur(Sprite):
     X_POS = 80
     Y_POS = 310
     JUMP_VELOCITY = 8.5
+    Y_POS_DUCK = 340
     def __init__(self):
         self.image = RUNNING[0]
         self.dino_rect = self.image.get_rect()
@@ -17,17 +18,28 @@ class Dinosaur(Sprite):
         
         self.running = True
         self.jumping = False
+        self.ducking = False
 
     def update(self, user_input):
         if self.running:
             self.run()
         elif self.jumping:
             self.jump()
+        elif self.ducking:
+            self.duck()
+
+        if self.step_index >= 10:
+            self.step_index = 0
         
         if user_input[pygame.K_UP] and not self.jumping:
             self.jumping = True
             self.running = False
-        elif not self.jumping:
+            self.ducking = False        
+        elif user_input[pygame.K_DOWN] and not self.jumping:
+            self.ducking = True
+            self.running = False
+            self.jumping = False
+        elif not (self.jumping or user_input[pygame.K_DOWN]):
             self.running = True
 
         if self.step_index >= 10:
@@ -40,13 +52,16 @@ class Dinosaur(Sprite):
     def jump(self):
         self.image = JUMPING
         self.dino_rect.y -= self.jump_velocity *4
-        print(self.dino_rect.y)
-        print(self.jump_velocity)
         self.jump_velocity -= 0.8
         if self.jump_velocity < -self.JUMP_VELOCITY:
             self.dino_rect.y = self.Y_POS
             self.jumping = False
             self.jump_velocity = self.JUMP_VELOCITY
+
+    def duck(self):
+        self.image = DUCKING[0] if self.step_index < 5 else  DUCKING[1] 
+        self.dino_rect.y = self.Y_POS_DUCK
+        self.step_index += 1 
 
     def draw(self, screen):
         screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
