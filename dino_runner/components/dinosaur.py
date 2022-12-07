1e41905c353d028e507746fcc8d5e952a39ad95b
PPAOLA12 <Paola.Paredes@fundacion-jala.org>
2022-12-06 22:10:41 -0400

    Class 2: Obstacles

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,6 +1,5 @@
 import pygame
 from pygame.sprite import Sprite
-
 from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING
 
 class Dinosaur(Sprite):
@@ -8,14 +7,12 @@ class Dinosaur(Sprite):
     Y_POS = 310
     JUMP_VELOCITY = 8.5
     Y_POS_DUCK = 340
+
     def __init__(self):
         self.image = RUNNING[0]
-        self.dino_rect = self.image.get_rect()
-        self.dino_rect.x = self.X_POS
-        self.dino_rect.y = self.Y_POS
+        self.reset_dino_rect()
         self.jump_velocity = self.JUMP_VELOCITY
         self.step_index = 0
-        
         self.running = True
         self.jumping = False
         self.ducking = False
@@ -28,30 +25,31 @@ class Dinosaur(Sprite):
         elif self.ducking:
             self.duck()
 
-        if self.step_index >= 10:
-            self.step_index = 0
-        
-        if user_input[pygame.K_UP] and not self.jumping:
-            self.jumping = True
-            self.running = False
-            self.ducking = False        
-        elif user_input[pygame.K_DOWN] and not self.jumping:
-            self.ducking = True
-            self.running = False
-            self.jumping = False
-        elif not (self.jumping or user_input[pygame.K_DOWN]):
-            self.running = True
+        if not self.jumping:
+            if user_input[pygame.K_UP]:
+                self.jumping = True
+                self.running = False
+                self.ducking = False
+            elif user_input[pygame.K_DOWN]:
+                self.ducking = True
+                self.running = False
+                self.jumping = False
+            else:
+                self.running = True
+                self.jumping = False
+                self.ducking = False
 
         if self.step_index >= 10:
             self.step_index = 0
-    
+
     def run(self):
         self.image = RUNNING[0] if self.step_index < 5 else RUNNING[1]
+        self.reset_dino_rect()
         self.step_index += 1
     
     def jump(self):
         self.image = JUMPING
-        self.dino_rect.y -= self.jump_velocity *4
+        self.dino_rect.y -= self.jump_velocity * 4
         self.jump_velocity -= 0.8
         if self.jump_velocity < -self.JUMP_VELOCITY:
             self.dino_rect.y = self.Y_POS
@@ -59,9 +57,15 @@ class Dinosaur(Sprite):
             self.jump_velocity = self.JUMP_VELOCITY
 
     def duck(self):
-        self.image = DUCKING[0] if self.step_index < 5 else  DUCKING[1] 
+        self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1]
+        self.reset_dino_rect()
         self.dino_rect.y = self.Y_POS_DUCK
-        self.step_index += 1 
+        self.step_index += 1
+
+    def reset_dino_rect(self):
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.X_POS
+        self.dino_rect.y = self.Y_POS
 
     def draw(self, screen):
         screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -1,9 +1,7 @@
 import pygame
-
+from dino_runner.components.obtacle_manager import ObstacleManager
 from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
-
 from dino_runner.components.dinosaur import Dinosaur
-
 
 class Game:
     def __init__(self):
@@ -18,6 +16,7 @@ class Game:
         self.y_pos_bg = 380
 
         self.player = Dinosaur()
+        self.obstacle_manager = ObstacleManager()
 
     def run(self):
         # Game loop: events - update - draw
@@ -36,12 +35,14 @@ class Game:
     def update(self):
         user_input = pygame.key.get_pressed()
         self.player.update(user_input)
+        self.obstacle_manager.update(self)
 
     def draw(self):
         self.clock.tick(FPS)
         self.screen.fill((255, 255, 255))
         self.draw_background()
         self.player.draw(self.screen)
+        self.obstacle_manager.draw(self.screen)
         pygame.display.update()
         pygame.display.flip()
 
@@ -49,7 +50,7 @@ class Game:
         image_width = BG.get_width()
         self.screen.blit(BG, (self.x_pos_bg, self.y_pos_bg))
         self.screen.blit(BG, (image_width + self.x_pos_bg, self.y_pos_bg))
-        if self.x_pos_bg <= -image_width:
+        if self.x_pos_bg <= -image_width: #la imagen paso el margen
             self.screen.blit(BG, (image_width + self.x_pos_bg, self.y_pos_bg))
             self.x_pos_bg = 0
         self.x_pos_bg -= self.game_speed
